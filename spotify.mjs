8ffd34e98092a41675d9f1cc20319250f85959b4
Alex <alexwerner9@gmail.com>
2024-05-01 22:12:39 -0400

    Search updates

--- spotify.mjs
+++ spotify.mjs
@@ -178,7 +178,7 @@ export async function search(access_token, query) {
         const data = await querySpotifyGet(access_token, '/search?'+q)
         let tracks;
         try {
-            tracks = data['tracks']['items'].slice(0,3)
+            tracks = data['tracks']['items'].slice(0,15)
         } catch(e) {
             console.log("ERROR", data)
             resolve({error: e})
